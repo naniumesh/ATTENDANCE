ebb79576d202a598eb05b636e04172cab63e3fdc
MALLEMKONDU UMESH <75903669+naniumesh@users.noreply.github.com>
2025-10-01 16:03:59 +0530

    Update admin-login.js

--- frontend/js/admin-login.js
+++ frontend/js/admin-login.js
@@ -8,7 +8,7 @@
       }
 
       try {
-        const res = await fetch("http://localhost:5000/api/admin/login", {
+        const res = await fetch("https://attendance-jmxr.onrender.com/api/admin/login", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({ username, password }),
